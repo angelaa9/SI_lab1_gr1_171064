58ea715187ec1087583c40a1922ef950a8eef8a9
angelaa9 <angelashumenkovska@yahoo.com>
2020-04-06 01:29:36 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -4,8 +4,37 @@ class Student {
 	String lastName;
 
 	//TODO constructor
+	ArrayList<String> labPoints = new ArrayList<String>( );
+
+	public Student(String index) {
+		this.index = index;
+	}
 
 	//TODO seters & getters
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
 
 	public double getAverage() {
 		//TODO
