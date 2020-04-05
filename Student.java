be430b13f086c8b07d0ec54c85749fde747a3c77
angelaa9 <angelashumenkovska@yahoo.com>
2020-04-06 01:42:33 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,12 +1,18 @@
+import java.util.*;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 
 	//TODO constructor
-	ArrayList<String> labPoints = new ArrayList<String>( );
-
-	public Student(String index) {
+	ArrayList<Integer> labPoints = new ArrayList<Integer>( );
+	Scanner scanner = new Scanner(System.in);
+	int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+		labPoints.insertLast(scanner.nextInt());
+}
+		public Student(String index) {
 		this.index = index;
 	}
 
@@ -38,9 +44,21 @@ class Student {
 
 	public double getAverage() {
 		//TODO
+		return labPoints/5;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+
+		if (labPoints.lenght >= 8)
+			return true;
+		else
+			return false;
+
 	}
+
+
+
+
+
 }
